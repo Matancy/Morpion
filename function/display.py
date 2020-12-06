cfcc33dfbcb959f20ad92e826c032b3b32c3b45e
Matancy <matheo.tichy@gmail.com>
2020-12-06 15:46:02 +0100

    Création de la fonction load_screen

--- function/display.py
+++ function/display.py
@@ -1,3 +1,5 @@
 import pygame
 
-def display(display):
+def load_screen(display):
+    img = pygame.image.load("data/start.jpg")
+    display.blit(img, (0, 0))
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
-#from function.display import *
+from function.display import *
 
 display = pygame.display.set_mode((900, 900))
 
 run = True
 while run:
-    img = pygame.image.load("data/start.jpg")
-    display.blit(img, (0, 0))
+    load_screen(display)
     pygame.display.flip()
 pygame.quit()
