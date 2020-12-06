b8562b1a221e6add19842e0c9170c7ba8422f257
Matancy <matheo.tichy@gmail.com>
2020-12-06 16:47:43 +0100

    Add function calcul
    + Ajout d'une fonction pour calculer dans quelle case on se trouve

---
+++ function/calculs.py
@@ -0,0 +1,21 @@
+def calcul_pos(x, y):
+    if x < 300 and y < 300:
+        return 0, 0
+    elif x > 300 and x < 600 and y < 300:
+        return 1, 0
+    elif x > 600 and x < 900 and y < 300:
+        return 2, 0
+    ###
+    elif x < 300 and y > 300 and y < 600:
+        return 0, 1
+    elif x > 300 and x < 600 and y > 300 and y < 600:
+        return 1, 1
+    elif x > 600 and x < 900 and y > 300 and y < 600:
+        return 2, 1
+    ###
+    elif x < 300 and y > 600 and y < 900:
+        return 0, 2
+    elif x > 300 and x < 600 and y > 600 and y < 900:
+        return 1, 2
+    elif x > 600 and x < 900 and y > 600 and y < 900:
+        return 2, 2
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from function.display import *
+from function.drawing import *
+from function.calculs import *
 
 display = pygame.display.set_mode((900, 900))
 
@@ -9,12 +11,27 @@ started = 0
 
 run = True
 while run:
+    # Lecture des évènements de Pygame
     for event in pygame.event.get():
+        # Fermeture du programme
         if event.type == pygame.QUIT:
             run = False
+
+        # Lecture des entrées clavier
         if event.type == pygame.KEYDOWN:
+            # Touche entrée
             if event.key == pygame.K_RETURN:
                 started = 1
+
+        # Lecture des entrées souris
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Clic gauche
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                pos = pygame.mouse.get_pos()
+                if started == 1:
+                    print(calcul_pos(pos[0], pos[1]))
+
+
     if started == 0:
         load_screen(display)
         pygame.display.flip()
