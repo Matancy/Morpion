55745f13aaa95e50e5e527c032d83408b8e8e0bc
Matancy <matheo.tichy@gmail.com>
2020-12-06 15:44:16 +0100

    Ecran d'accueil fonctionnel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+#from function.display import *
+
+display = pygame.display.set_mode((900, 900))
 
 run = True
 while run:
-    display = pygame.display.set_mode((900, 900))
     img = pygame.image.load("data/start.jpg")
     display.blit(img, (0, 0))
     pygame.display.flip()
