f8e7428d16368c4dafda6360d61b9a33dcfb077d
Matancy <matheo.tichy@gmail.com>
2020-12-07 16:48:43 +0100

    Reduce circle
    * Reduce circle size

--- function/drawing.py
+++ function/drawing.py
@@ -3,31 +3,32 @@ import pygame
 
 def circle(display, cords):
     if cords[0] == 0 and cords[1] == 0:
-        pygame.draw.circle(display, (0, 0, 0), (150, 150), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (150, 150), 75, 2)
         pygame.display.flip()
     elif cords[0] == 1 and cords[1] == 0:
-        pygame.draw.circle(display, (0, 0, 0), (450, 150), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (450, 150), 75, 2)
         pygame.display.flip()
     elif cords[0] == 2 and cords[1] == 0:
-        pygame.draw.circle(display, (0, 0, 0), (750, 150), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (750, 150), 75, 2)
         pygame.display.flip()
     elif cords[0] == 0 and cords[1] == 1:
-        pygame.draw.circle(display, (0, 0, 0), (150, 450), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (150, 450), 75, 2)
         pygame.display.flip()
     elif cords[0] == 1 and cords[1] == 1:
-        pygame.draw.circle(display, (0, 0, 0), (450, 450), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (450, 450), 75, 2)
         pygame.display.flip()
     elif cords[0] == 2 and cords[1] == 1:
-        pygame.draw.circle(display, (0, 0, 0), (750, 450), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (750, 450), 75, 2)
         pygame.display.flip()
     elif cords[0] == 0 and cords[1] == 2:
-        pygame.draw.circle(display, (0, 0, 0), (150, 750), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (150, 750), 75, 2)
         pygame.display.flip()
     elif cords[0] == 1 and cords[1] == 2:
-        pygame.draw.circle(display, (0, 0, 0), (450, 750), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (450, 750), 75, 2)
         pygame.display.flip()
     elif cords[0] == 2 and cords[1] == 2:
-        pygame.draw.circle(display, (0, 0, 0), (750, 750), 100, 2)
+        pygame.draw.circle(display, (0, 0, 0), (750, 750), 75, 2)
+        pygame.display.flip()
 
 
 def cross(display, cords):
