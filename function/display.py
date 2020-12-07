087f47a95bc8c53e96ba7ee7ca66e221ff945fb4
Matancy <matheo.tichy@gmail.com>
2020-12-07 16:21:40 +0100

    Fix indent problem
    * Change indentation shown by the IDE

--- function/display.py
+++ function/display.py
@@ -5,6 +5,7 @@ def load_screen(display):
     img = pygame.image.load("data/start.jpg")
     display.blit(img, (0, 0))
 
+
 def paint_grid(display):
     display.fill((255, 255, 255))
     pygame.draw.line(display, (0, 0, 0), (0, 300), (900, 300), 2)
