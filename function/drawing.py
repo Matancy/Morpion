db02b7e8e985a73e1f1a13a07964694589ede1bd
Matancy <matheo.tichy@gmail.com>
2020-12-07 16:21:13 +0100

    Add circle functtion
    + Add functionnal circle function

--- function/drawing.py
+++ function/drawing.py
@@ -1,3 +1,7 @@
 import pygame
 
-#def circle():
+
+def circle(display, cords):
+    if cords[0] == 0 and cords[1] == 0:
+        pygame.draw.circle(display, (0, 0, 0), (150, 150), 100, 2)
+        pygame.display.flip()
